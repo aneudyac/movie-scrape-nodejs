ab962119787736f42c7a67502b472ced88436d12
Aneudy Abreu <aneudy91@gmail.com>
2022-09-18 21:10:03 -0500

    fix cuevana

--- services/cuevana.js
+++ services/cuevana.js
@@ -2,7 +2,7 @@ import fetch from "node-fetch";
 import cheerio from "cheerio";
 import { chromium } from "playwright";
 
-export const getTomatomatelaSrc = async url => {
+const getTomatomatelaSrc = async url => {
   try {
     const response = await fetch(url);
     const body = await response.text();
@@ -17,7 +17,7 @@ export const getTomatomatelaSrc = async url => {
   }
 };
 
-export const getTomatomatelaEmbed = async urlPrm => {
+const getTomatomatelaEmbed = async urlPrm => {
   return new Promise(async (resolve, reject) => {
     const browser = await chromium.launch({ headless: true });
     const context = await browser.newContext();
@@ -28,8 +28,8 @@ export const getTomatomatelaEmbed = async urlPrm => {
       totalLoaded++;
       const _redirectUrl = page.url();
 
+      console.log("page load", { _redirectUrl });
       if (_redirectUrl?.indexOf("embed.html") != -1) {
-        console.log("page load", { _redirectUrl });
         resolve(_redirectUrl);
       }
 
@@ -55,7 +55,7 @@ export const getTomatomatelaEmbed = async urlPrm => {
   });
 };
 
-export const getFile = async url => {
+const getFile = async url => {
   try {
     console.log({ url });
     const response = await fetch(url);
@@ -67,16 +67,17 @@ export const getFile = async url => {
   }
 };
 
-export const testRedirect = () => {
-  // const request = https.request(
-  //   new URL(
-  //     "https://apialfa.tomatomatela.com/ir/goto.php?h=bHJMRW1oTVhldDJJZnZnUE10V3NBQmx5b2ZqemljRFk1RmUybGphTUI2ZFhTWTNzZkUwOXAza0NTSktMWW9vQ1pWSXViY2JaOEdPazE4bjBVS2NMUXhlRWUwWWhxU0ZjdGNIT0dBWFI5U2VWdTJaK0ZiTDQ4UGVXckcwTG5RcEpmRTVLZlZCVE9OTHVlQVBOa3FrSHdNVCtOZTBHSnpFQWFvN0kvL2d2Ty9SUjdxMitPcnNQbi8rVk53cHZWVTNLaDJQWlJHYWgvNFl2aGNsb09NdnQ1b3I4bm1kRjJKWkY3WTV2Z3pwNTlvM3NWNWdCM3VKZWV6VEFDOEJ4R1BRbXF5NWFrU0RVU2FPd1FLSUZRaWhlU0IzVmI0M3FsNUM2OC92bC9CaUN3QXVYZmZZSGdvbFI5SktISUxVWE9zWWQ3cTlaeFlVOVZSeFAvTXBVRXl5NXF3PT0"
-  //   ),
-  //   // new URL("https://bitly.com/UHfDGO"),
-  //   response => {
-  //     console.log(response.responseUrl);
-  //     // 'http://duckduckgo.com/robots.txt'
-  //   }
-  // );
-  // request.end();
+export default async (url, headers) => {
+  try {
+    let src = await getTomatomatelaSrc(url);
+    const embed = await getTomatomatelaEmbed(src);
+
+    let file = embed.split("#")[1];
+    let checkUrl = "https://tomatomatela.com/details.php?v=" + file;
+
+    const data = await getFile(checkUrl);
+    return Promise.resolve(data.file);
+  } catch (error) {
+    return Promise.reject(error);
+  }
 };
